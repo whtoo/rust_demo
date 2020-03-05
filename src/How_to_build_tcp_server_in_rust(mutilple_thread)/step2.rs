b30d1e6d04547c4d35fcab76681fbfab5a83188d
blitz.whtoo <whtoo@qq.com>
2020-03-05 17:51:01 +0800

    1. 增加多线程demo

--- src/How_to_build_tcp_server_in_rust(mutilple_thread)/step2.rs
+++ src/How_to_build_tcp_server_in_rust(mutilple_thread)/step2.rs
@@ -3,20 +3,17 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
 use std::thread;
-use rustdemo::ThreadPool;
 use std::time::Duration;
 
 fn main()  {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(stream);
+        thread::spawn( || {
+            handle_connection(stream)
         });
     }
-    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
